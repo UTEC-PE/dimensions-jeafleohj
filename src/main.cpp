6be3a3b64a4129f0a83429d10049603bc13d12ef
jeafleohj <jhony.angulo@utec.edu.pe>
2018-09-15 18:15:58 -0500

    Add output test

--- src/main.cpp
+++ src/main.cpp
@@ -26,14 +26,10 @@ struct Integer {
 int main(int argc, char *argv[]) {
     system("clear");
 	const int dim = 2;
-	int m[] = {2,2,3,4,3};
-		
-	for(int i=0; i<dim; i++){
-		m[i] = i;
-	}
-	Vector<Integer> v(dim, m);
+	int m[] = {2,2,3,4,3}, *pm = m;
+	Vector<Integer> v(4, m);
 	int k[] = {1,1,2,3,4}, *pk=k;
 	v.set(6, pk);
-	cout << v.get(pk) << "M\n";
+	cout << v.get(pk) << "\n";
     return EXIT_SUCCESS;
 }
